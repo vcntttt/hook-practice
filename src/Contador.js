6c9237eb4c024228688ebbbd7c43029e1e46bab6
Vicente Rivera <vicenterivera225@gmail.com>
2022-10-28 18:06:29 -0300

    codigo formateado

--- src/Contador.js
+++ src/Contador.js
@@ -3,32 +3,30 @@ import { useState } from "react";
 
 const Contador = () => {
   const [cuenta, setCuenta] = useState(100);
-;
+  const aumentar = () => {
+    setCuenta(cuenta + 1);
+  };
+  const disminuir = () => {
+    setCuenta(cuenta - 1);
+  };
 
-const aumentar = () => {
-  setCuenta(cuenta + 1);
-};
-const disminuir = () => {
-    setCuenta(cuenta - 1)
-};
+  const reset = () => {
+    setCuenta(100);
+  };
 
-const reset = () => {
-    setCuenta(100)
-};
-
-useEffect(() =>{
+  useEffect(() => {
     console.log(`El contador va en ${cuenta}`);
-}, [cuenta] )
+  }, [cuenta]);
 
-return (
+  return (
     <div>
-        <center>
-            <h1>{cuenta}</h1>
-            <button onClick={aumentar} >Aumentar</button>
-            <button onClick={disminuir} >Disminuir</button>
-            <button onClick={reset} >Reset</button>
-        </center>
+      <center>
+        <h1>{cuenta}</h1>
+        <button onClick={aumentar}>Aumentar</button>
+        <button onClick={disminuir}>Disminuir</button>
+        <button onClick={reset}>Reset</button>
+      </center>
     </div>
-)
+  );
 };
 export default Contador;
